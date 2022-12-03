c79fad88b56a20ad747a5cc4136ea026052e7efb
Prasiddha <shakyaprasiddha8@gmail.com>
2022-12-03 18:46:14 +0545

    delete selected item form list

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,21 +1,40 @@
-
-import {useState} from'react'
+// import Button from "../components/button"
+import {useState} from 'react'
 const Login =()=> {
-  const [arr,setArr]=useState(['ram','hari','shyam','geeta'])
-  const popLastItem=()=>{
-    // backup is created as useState can't be manipulated with pop()
-    const backup=[...arr]
-    backup.pop()
-    setArr(backup)
+  const [arr,setArr] = useState(['ram','hari','geeta','shyam']);
+  const [inputText,setInputText] = useState('')
+  const deleteItem =()=> {
+  const removedArr =arr.filter(item=>{
+       return item!=inputText
+     })
+     setArr(removedArr)
+};
+
+
+  const saveName=(e)=>{
+   setInputText(e.target.value)
+    
   }
+
   return (
     <>
-    {arr.map((item,id)=>{
-       return <li>{item}</li>
-    })   
-    }
-    <button onClick={()=>popLastItem()}>POP</button>
+    {arr.map((item,id)=> {
+      return <li>{item}</li>;
+    })}
+  <button onClick ={() => deleteItem()}>delete</button>
+
+  <input
+        type="text"
+        placeholder={'enter name you want to delete'}
+        onKeyUp={saveName}
+        
+        ></input>
+    
    </>
   );
-}
+};
+  
+
+
+
 export default Login;
